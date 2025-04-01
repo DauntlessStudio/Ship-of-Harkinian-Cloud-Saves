27977a66d56c3585180cb35ffc1f5e1c9123db9e
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2025-04-01 16:22:15 -0500

    v1.0.0

--- src/compile.ts
+++ src/compile.ts
@@ -1,23 +1,23 @@
 new Deno.Command("deno", {
-    args: ["compile", "-A", "--target", "x86_64-pc-windows-msvc", "-o", "dist/server.exe", "src/server/server.ts"],
+    args: ["compile", "-A", "--target", "x86_64-pc-windows-msvc", "-o", "dist/x86_64-windows-server.exe", "src/server/server.ts"],
     stderr: "inherit",
     stdout: "inherit",
 }).outputSync();
 
 new Deno.Command("deno", {
-    args: ["compile", "-A", "--target", "x86_64-pc-windows-msvc", "-o", "dist/client.exe", "src/client/client.ts"],
+    args: ["compile", "-A", "--target", "x86_64-pc-windows-msvc", "-o", "dist/x86_64-windows-client.exe", "src/client/client.ts"],
     stderr: "inherit",
     stdout: "inherit",
 }).outputSync();
 
 new Deno.Command("deno", {
-    args: ["compile", "-A", "--target", "x86_64-unknown-linux-gnu", "-o", "dist/server", "src/server/server.ts"],
+    args: ["compile", "-A", "--target", "x86_64-unknown-linux-gnu", "-o", "dist/x86_64-linux-server", "src/server/server.ts"],
     stderr: "inherit",
     stdout: "inherit",
 }).outputSync();
 
 new Deno.Command("deno", {
-    args: ["compile", "-A", "--target", "x86_64-unknown-linux-gnu", "-o", "dist/client", "src/client/client.ts"],
+    args: ["compile", "-A", "--target", "x86_64-unknown-linux-gnu", "-o", "dist/x86_64-linux-client", "src/client/client.ts"],
     stderr: "inherit",
     stdout: "inherit",
 }).outputSync();
